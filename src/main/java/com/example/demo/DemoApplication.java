22c01b1ceaae56fb85dd2859e849e7dda32858f4
bala-amypo <bala@amypo.in>
2025-12-22 08:22:48 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,22 +1,12 @@
-package com.example.demo.servlet;
+package com.example.demo;
 
-import java.io.IOException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+@SpringBootApplication
+public class DemoApplication {
 
-public class SimpleHelloServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setStatus(200);
-        resp.setContentType("text/plain");
-        resp.getWriter().write("Hello from Simple Servlet");
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setStatus(200);
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
--- src/main/java/com/example/demo/exception/ConflictException.java
+++ src/main/java/com/example/demo/exception/ConflictException.java
@@ -2,6 +2,10 @@ package com.example.demo.exception;
 
 public class ConflictException extends RuntimeException {
 
+    public ConflictException() {
+        super();
+    }
+
     public ConflictException(String message) {
         super(message);
     }
