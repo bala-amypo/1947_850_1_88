6b6b23d0e21d4832539c64f5675e722d339eaca7
bala-amypo <bala@amypo.in>
2025-12-22 08:10:49 +0000

    User commit

--- src/main/java/com/example/demo/exception/ConflictException.java
+++ src/main/java/com/example/demo/exception/ConflictException.java
@@ -1,5 +1,8 @@
 package com.example.demo.exception;
 
-public class ConflictException {
+public class ConflictException extends RuntimeException {
 
+    public ConflictException(String message) {
+        super(message);
+    }
 }
