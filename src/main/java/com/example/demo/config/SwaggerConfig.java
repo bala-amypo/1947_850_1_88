86266fd7fac873c6de9cfffdd43757c85e7675fe
bala-amypo <bala@amypo.in>
2025-12-25 11:28:06 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -7,27 +7,30 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
 
-        @Bean
-        public OpenAPI customOpenAPI() {
+    @Bean
+    public OpenAPI customOpenAPI() {
 
         SecurityScheme securityScheme = new SecurityScheme()
                 .type(SecurityScheme.Type.HTTP)
                 .scheme("bearer")
                 .bearerFormat("JWT");
 
+        Server server = new Server()
+                .url("https://9248.408procr.amypo.ai/")
+                .description("Production Server");
+
         return new OpenAPI()
-        
+                .addServersItem(server)
                 .components(
                         new Components().addSecuritySchemes("bearerAuth", securityScheme)
                 )
                 .addSecurityItem(
                         new SecurityRequirement().addList("bearerAuth")
                 );
-        
-
-        }
+    }
 }
